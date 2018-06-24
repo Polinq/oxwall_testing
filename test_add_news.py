3e56521e254548358a2524643b8b9e86b6576e02
Olena Piankova <el.piankova@gmail.com>
2018-06-24 22:21:00 +0300

    Modify test to pytest

--- test_add_news.py
+++ test_add_news.py
@@ -1,28 +1,24 @@
-import unittest, time
+import pytest
 from oxwall_application import OxwallApp
 
 
-class AddNewsTestCase(unittest.TestCase):
-    def setUp(self):
-        self.app = OxwallApp(base_url="https://demo.oxwall.com/")
-
-    def test_add_text_news(self):
-        user = "demo"
-        password = "demo"
-        text_news = "New news!"
-        app = self.app
-
-        app.login(user, password)
-        old_list_of_news = app.get_list_of_news()
-        app.add_new_news(text_news)
-        app.wait_new_news_appearing(old_list_of_news)
-        self.assertEqual(text_news, app.last_news_text_element().text)
-        self.assertEqual(user.title(), app.last_news_user_element().text)
-        app.logout(user)
-
-    def tearDown(self):
-        self.app.close()
+@pytest.fixture()
+def app():
+    app = OxwallApp(base_url="https://demo.oxwall.com/")
+    yield app
+    app.close()
 
 
-if __name__ == "__main__":
-    unittest.main()
+def test_add_text_news(app):
+    user = "demo"
+    password = "demo"
+    text_news = "New news!"
+
+    app.login(user, password)
+    old_list_of_news = app.get_list_of_news()
+    app.add_new_news(text_news)
+    app.wait_new_news_appearing(old_list_of_news)
+    assert text_news == app.last_news_text_element().text
+    assert user.title() == app.last_news_user_element().text
+    app.logout(user)
+
