a8085d3f2756b9de033a1c493840a2b03367c4a0
Olena Piankova <el.piankova@gmail.com>
2018-06-25 23:01:12 +0300

    Extracted login as a separate fixture

--- test_add_news.py
+++ test_add_news.py
@@ -8,17 +8,18 @@ def app():
     yield app
     app.close()
 
-
-def test_add_text_news(app):
+@pytest.fixture()
+def logged_user(app):
     user = "demo"
     password = "demo"
-    text_news = "New news!"
-
     app.login(user, password)
+    yield user
+    app.logout(user)
+
+def test_add_text_news(app, logged_user):
+    text_news = "New news!"
     old_list_of_news = app.get_list_of_news()
     app.add_new_news(text_news)
     app.wait_new_news_appearing(old_list_of_news)
     assert text_news == app.last_news_text_element().text
-    assert user.title() == app.last_news_user_element().text
-    app.logout(user)
-
+    assert logged_user.title() == app.last_news_user_element().text
