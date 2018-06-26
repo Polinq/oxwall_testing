60c1946ec366b966ebb6805378608eea23bd6a26
Olena Piankova <el.piankova@gmail.com>
2018-06-26 19:14:16 +0300

    Added config file

--- conftest.py
+++ conftest.py
@@ -1,19 +1,25 @@
 import pytest
 from oxwall_application import OxwallApp
 from models.user import User
+import json
 
+
+@pytest.fixture(scope="session")
+def config():
+    with open("config.json") as f:
+        return json.load(f)
 
 
 @pytest.fixture()
-def app():
-    app = OxwallApp(base_url="https://demo.oxwall.com/")
+def app(config):
+    app = OxwallApp(base_url=config["base_url"])
     yield app
     app.close()
 
 
 @pytest.fixture()
-def logged_user(app):
-    user = User(username="demo", password="demo")
+def logged_user(app, config):
+    user = User(**config["admin"])
     app.login(user)
     yield user
     app.logout(user)
